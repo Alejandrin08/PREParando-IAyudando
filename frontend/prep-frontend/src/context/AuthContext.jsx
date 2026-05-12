781a36cdb6dcc35b09660459985d595db8884866
Alejandrin08 <zs21013870@estudiantes.uv.mx>
2026-05-12 13:38:07 -0600

    Fixed bug

--- frontend/prep-frontend/src/context/AuthContext.jsx
+++ frontend/prep-frontend/src/context/AuthContext.jsx
@@ -17,7 +17,7 @@ export function AuthProvider({ children }) {
             token,
             email: decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
             fullName: decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
-            role: decoded['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role'],
+            role: decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'],
           })
         } else {
           localStorage.removeItem('token')
